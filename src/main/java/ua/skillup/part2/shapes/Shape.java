c04915906b02cd5c31ce12ad60b7cb2bd97b3c5b
Viktor Maksymenko <vikmaksimenko@hotmail.com>
2024-03-19 21:47:33 +0100

    Adding shapes solution

---
+++ src/main/java/ua/skillup/part2/shapes/Shape.java
@@ -0,0 +1,7 @@
+package ua.skillup.part2.shapes;
+
+public abstract class Shape {
+    abstract public double perimeter();
+
+    abstract public double area();
+}
---
+++ src/main/java/ua/skillup/part2/shapes/Square.java
@@ -0,0 +1,7 @@
+package ua.skillup.part2.shapes;
+
+public class Square extends Rectangle {
+    public Square(double a) {
+        super(a, a);
+    }
+}
