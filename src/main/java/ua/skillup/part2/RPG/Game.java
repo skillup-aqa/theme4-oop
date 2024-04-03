a2b7fd75fe4f4d3044c1488801273753efac2415
Sasha2511 <nedzelnytskyis@gmail.com>
2024-04-03 10:49:18 +0300

    Iteration 06 of RPG

--- src/main/java/ua/skillup/part2/RPG/Game.java
+++ src/main/java/ua/skillup/part2/RPG/Game.java
@@ -2,13 +2,12 @@ package ua.skillup.part2.RPG;
 
 public class Game {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         GameDriver game = new GameDriver();
-        game.raund(game.magician);
-        game.raund(game.warrior);
-        game.raund(game.archer);
-        game.raund(game.archer);
+        game.round(game.archer);
+
 
         game.magician.display();
+        game.ork.display();
     }
 }
--- src/main/java/ua/skillup/part2/RPG/GameDriver.java
+++ src/main/java/ua/skillup/part2/RPG/GameDriver.java
@@ -11,15 +11,14 @@ public class GameDriver {
 
     public static void kick(Unit kicker, Unit kicked) {
         int minusHealth = kicker.kickPower;
-        if (kicked.isPoisoned) {
+        if (kicked.isPoisoned && kicked.unitName.equals("Ork")) {
             //the case when archer is used three times in a row is not considered
             minusHealth = kicker.kickPower + kicker.kickPower / 2;
+            kicked.setPoisoned(false);
         }
         kicked.changeHealth(minusHealth);
         if (kicker.unitName.equals("Archer")) {
             kicked.setPoisoned(true);
-        } else {
-            kicked.setPoisoned(false);
         }
         if (kicker.unitName.equals("Magician")) {
             kicker.magicianMadeKick();
@@ -27,14 +26,14 @@ public class GameDriver {
         }
     }
 
-    public void raund(Unit kicker) {
+    public void round(Unit kicker) {
         magician.magicianAddCounter();
         if (magician.getCounter() > 2) {
             magician.kickPower = 80;
         }
         randomSoldier = arrayOfSoldiers[(int) (Math.random() * 3)];
-        kick(ork, randomSoldier);
         kick(kicker, ork);
+        kick(ork, randomSoldier);
         ork.display();
         randomSoldier.display();
         kicker.display();
