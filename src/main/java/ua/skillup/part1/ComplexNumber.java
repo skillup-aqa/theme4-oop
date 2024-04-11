4e0c50b86f02142b1775d240eeae409eff60dca7
Tetiana Yakymiuk <t882005@yahoo.com>
2024-04-10 23:31:45 -0400

    Resolving Complex Number

--- src/main/java/ua/skillup/part1/ComplexNumber.java
+++ src/main/java/ua/skillup/part1/ComplexNumber.java
@@ -1,58 +1,90 @@
 package ua.skillup.part1;
 
+import java.util.concurrent.Callable;
+
 public class ComplexNumber {
+    private double real;
+    private double imaginary;
+
     public ComplexNumber(double real, double imaginary) {
-        // TODO: Implement constructor here
+        this.real = real;
+        this.imaginary = imaginary;
     }
 
     public double getReal() {
-        // TODO: Implement method here
-        return -1.0;
+        return this.real;
     }
 
     public double getImaginary() {
-        // TODO: Implement method here
-        return -1.0;
+        return this.imaginary;
     }
 
     public double abs() {
-        // TODO: Implement method here
-        return -1.0;
+        return Math.sqrt(real * real + imaginary * imaginary);
     }
 
     public ComplexNumber add(ComplexNumber other) {
-        // TODO: Implement method here
-        return null;
+        return new ComplexNumber(real + other.real, imaginary + other.imaginary);
     }
 
     public ComplexNumber subtract(ComplexNumber other) {
-        // TODO: Implement method here
-        return null;
+        return new ComplexNumber(real - other.real, imaginary - other.imaginary);
     }
 
     public ComplexNumber multiply(ComplexNumber other) {
-        // TODO: Implement method here
-        return null;
+        return new ComplexNumber(real * other.real - imaginary * other.imaginary,
+                real * other.imaginary + imaginary * other.real);
     }
 
     public ComplexNumber multiply(double factor) {
-        // TODO: Implement method here
-        return null;
+        return new ComplexNumber(real * factor, imaginary * factor);
     }
 
     public ComplexNumber divide(ComplexNumber other) {
-        // TODO: Implement method here
-        return null;
+        double denominator = other.real * other.real + other.imaginary * other.imaginary;
+        return new ComplexNumber((real * other.real + imaginary * other.imaginary) / denominator,
+                (imaginary * other.real - real * other.imaginary) / denominator);
     }
 
     public ComplexNumber divide(double divisor) {
-        // TODO: Implement method here
-        return null;
+        return new ComplexNumber(real/divisor, imaginary/divisor);
     }
 
     public ComplexNumber conjugate() {
-        // TODO: Implement method here
-        return null;
+        return new ComplexNumber(real, - imaginary);
     }
+    public String toString() {
+        return real + "+" + imaginary + "i";
+    }
+    public static void main(String[] args) {
+        ComplexNumber a = new ComplexNumber(1, 5);
+        ComplexNumber b = new ComplexNumber(3, 7);
 
+        double abs = a.abs();
+        System.out.println("Absolute value " + a.toString() + " is " + abs);
+
+        ComplexNumber c = a.add(b);
+        System.out.println("Adding a complex number: " +
+                a.toString() + "+" + b.toString() + "=" + c.toString());
+
+        c = a.subtract(b);
+        System.out.println("Subtraction of a complex number: " + a.toString() + "-" + b.toString() + "=" + c.toString());
+
+        c = a.multiply(b);
+        System.out.println("Multiplication by a complex number: " + a.toString() + " * " + b.toString() + " = " + c.toString());
+
+        c = a.multiply(2);
+        System.out.println("Multiplication by number: " + a.toString() + " * 2 = " + c.toString());
+
+        c = a.divide(b);
+        System.out.println("Division by a complex number: " + a.toString() + " / " + b.toString() + " = " + c.toString());
+
+        c = a.divide(2);
+        System.out.println("Division by number: " + a.toString() + " / 2 = " + c.toString());
+
+        c = a.conjugate();
+        System.out.println("The conjugate of " + a.toString() + " is " + c.toString());
+
+    }
 }
+
